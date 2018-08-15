3759c49c73232998f811aa3ac2f22c01b654f8e2
Bruce How <30928213+Brucehow4@users.noreply.github.com>
2018-08-15 12:13:52 +0800

    Add more solutions

--- lab2/asterisks.c
+++ lab2/asterisks.c
@@ -23,5 +23,36 @@ int main(int argc, char *argv[]) {
 		}	
 		printf("\n");
 	}
+	// q6 b
+	for(int i = 4; i >= 0; i--) {
+		for(int j = 0; j < i; j++) {
+			printf(" ");
+		}
+		for(int k = 0; k < 5-i; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	// q6 c
+	for(int i = 5; i > 0; i--) {
+		for(int k = i; k > 0; k--) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	// q6 d
+	int count = 1;
+	for(int i = 4; i >= 0; i--) {
+		for(int k = 0; k < i; k++) {
+			printf(" ");
+		}
+		for(int m = 0; m < count; m++) {
+			printf("*");
+		}
+		printf("\n");
+		count += 2;
+	}
 
 }
